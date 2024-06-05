b50777757ff98e1b81495673f26837f0715f24f5
Achrafsoukrati <achrafsoukrati90@gmail.com>
2024-06-05 10:06:18 +0100

    first commit

---
+++ src/players.js
@@ -0,0 +1,35 @@
+export const player = [
+    {
+        name: "Lamine Yamal",
+        team: "FC Barcelona",
+        nationality: "Spanish",
+        jerseyNumber: 27,
+        age: 34,
+        imageURL: "https://www.sofoot.com/convert/LzIwMjMvMDgvMjAxOTM4MjcvNjIwNjUzNi1oZC0xNDAweDkzMy5qcGc/image/lamine-yamal-devient-le-plus-jeune-joueur-de-lhistoire-du-barca-a-debuter-un-match-officiel.jpg"
+      },
+      {
+        name: "Pedri",
+        team: "FC Barcelona",
+        nationality: "Spanish",
+        jerseyNumber: 20,
+        age: 34,
+        imageURL: "https://images2.minutemediacdn.com/image/upload/c_fill,w_1440,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/FC-Barcelona-v-Real-Sociedad---La-Liga-Santander-03bd8940a359625a17aae4ec0473ba8e.jpg"
+      },
+      {
+        name: "Gavi",
+        team: "FC Barcelona",
+        nationality: "Spanish",
+        jerseyNumber: 30,
+        age: 19,
+        imageURL: "https://assets-fr.imgfoot.com/media/cache/1200x900/gavi-fc-barcelone-fm.jpg"
+      },
+      {
+        name: "Raphinia",
+        team: "FC Barcelona",
+        nationality: "Brazil",
+        jerseyNumber: 11,
+        age: 29,
+        imageURL: "https://photo.maxifoot.fr/raphinha-fc-barcelone-3.jpg"
+      },
+]
+
